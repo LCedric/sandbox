e7ac426b1a9315aa3538ca37de7d5c22fa9e520c
Leroy Cedric <131438421+LCedric@users.noreply.github.com>
2023-11-25 19:15:53 +0100

    Initial commit

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/angular';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  it('should create the app', async () => {
+    await render(AppComponent);
+  });
+});
