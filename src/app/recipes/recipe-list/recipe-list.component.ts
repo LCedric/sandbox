a4ee1a3ba7d20bae61dc849c4374e2edb401cd03
LCedric <Leroy_Cedric@outlook.com>
2023-11-26 20:01:46 +0100

    recipe item layout

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Recipe } from '../models/recipe.model';
 import { RecipeItemComponent } from './recipe-item/recipe-item.component';
 
 @Component({
@@ -9,4 +10,15 @@ import { RecipeItemComponent } from './recipe-item/recipe-item.component';
   styleUrl: './recipe-list.component.scss',
   imports: [CommonModule, RecipeItemComponent],
 })
-export class RecipeListComponent {}
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    {
+      name: 'A Test Recipe',
+      description: 'This is a test',
+      imagePath:
+        'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg?20170213105318',
+    },
+  ];
+
+  constructor() {}
+}
